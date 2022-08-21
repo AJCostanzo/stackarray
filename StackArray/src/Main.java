75f10611256da5a4ba9ea85b086939875189616e
Alexander Costanzo <ajcostanzo@gmail.com>
2022-08-21 15:03:54 -0400

    Alexander Costanzo Work in progress

---
+++ StackArray/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    private int length;
+    private int topOfStack;
+    private int[] myStack = new int[length];
+
+    public Main () {
+        this.length = length;
+        this.topOfStack = topOfStack;
+    }
+    public void push(int x) {
+        myStack[topOfStack++] = x;
+    }
+}
